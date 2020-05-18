ac6bfe2d29ba14510d4d9ba00a68ae8a01c9a0ea
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-05-18 10:40:37 -0300

    criado loader dos models da aplicação

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Davi Prada Hoffmann',
+    email: 'davi.hoffmann@unidavi.edu.br',
+    password_hash: '4654654645',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
