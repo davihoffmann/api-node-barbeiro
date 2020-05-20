7da90ace4c491f9caf99b4007ec7c1d0e5ca7612
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-05-20 10:45:29 -0300

    adicionado função para enviar email ao cancelar um appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f7ffb2b8712e98',
+    pass: 'cbd145c5d61f1d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
